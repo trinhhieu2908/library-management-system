993d6ed0c23cba6f3750575f4ad4587c148fe7d9
Dylan Tran <trinhhieu.tran.au@gmail.com>
2025-09-21 16:47:41 +1000

    deploy

--- TechVidvanLibrary/settings.py
+++ TechVidvanLibrary/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['prt582-library-management-system.onrender.com', 'localhost', '127.0.0.1']
 
+# CSRF settings for Render deployment
+CSRF_TRUSTED_ORIGINS = ['https://prt582-library-management-system.onrender.com']
 
 # Application definition
 
